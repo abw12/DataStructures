c19a3046463a35caedcb9c4a913af19f59990d15
Abhishek Wasave <Abhi_wasave@abw12>
2025-07-24 22:58:14 +0530

    Determine if two strings are close [Medium]

---
+++ src/LeetCode/Array/DeterminIfTwoStringAreClose.java
@@ -0,0 +1,46 @@
+package LeetCode.Array;
+
+import java.util.Arrays;
+
+public class DeterminIfTwoStringAreClose {
+
+    private static boolean compareString(String word1,String word2){
+        // condition1 - both strings length should match
+        if(word1.length() != word2.length()) return false;
+
+        //count the freq of character for both string
+        int[] freq1= new int[26];
+        int[] freq2= new int[26];
+
+        for(char ch: word1.toCharArray())
+            freq1[ch - 'a']++;
+
+        for(char ch:word2.toCharArray())
+            freq2[ch - 'a']++;
+
+        //condition 2 - should contain same set of characters
+        for(int i=0; i < 26 ;i++){
+            if((freq1[i] == 0 ) != (freq2[i] == 0)){
+                return false;
+            }
+        }
+        //condition 3 - sort and then compare the frequencies it should match (same multiset of frequencies)
+        // Only the non-zero entries matter; log 26 ≈ constant, so still O(1) extra time.
+//        for(int i=0; i < 26 ;i++){
+//            if(freq1[i] != freq2[i]){
+//                return false;
+//            }
+//        }
+//        return true;
+        return Arrays.equals(freq1,freq2);
+    }
+
+
+    public static void main(String[] args) {
+        String word1 = "cabbba";
+        String word2 = "abbccc";
+
+        System.out.println(compareString(word1,word2)
+        );
+    }
+}
